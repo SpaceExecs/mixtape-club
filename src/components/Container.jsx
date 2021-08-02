bed822dd46af8267f5ba4d45e8bb65c6987c9af0
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-08-02 17:13:47 -0500

    linting edits and clean up;

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,41 +1,128 @@
-import React from "react";
-import { Switch, Route, withRouter, Redirect } from "react-router-dom";
+import React from 'react';
+import {
+  Switch, Route, withRouter, Redirect,
+} from 'react-router-dom';
 
-import Landing from "./Landing.jsx";
-import Login from "./Login.jsx";
-import CreateMixtapes from "./CreateMixtapes.jsx";
-import MixtapePlayer from "./MixtapePlayer.jsx";
+import Landing from './Landing';
+import Login from './Login';
+import CreateMixtapes from './CreateMixtapes';
+import MixtapePlayer from './MixtapePlayer';
 
 /** Container component handles the front-end routing/rendering of the app and renders the Login,
- * CreateMixtapes, and MixtapePlayer components at their respective routes. Container is a child 
+ * CreateMixtapes, and MixtapePlayer components at their respective routes. Container is a child
  * component of App.
  */
 
 function Container(props) {
+  const {
+    isAuthenticated,
+    authenticateUser,
+    location,
+    searchResults,
+    onChange,
+    onSearch,
+    onPlayVideo,
+    onReady,
+    onPauseVideo,
+    onResultClick,
+    playing,
+    selectedResult,
+    tapeImages,
+    builderImage,
+    selectImage,
+    tapeLabel,
+    onLabelChange,
+    onPassToSideA,
+    sideA,
+    onPassToSideB,
+    sideB,
+    displayImageSelector,
+    onSaveImage,
+    onDeckSideA,
+    onDeckSideB,
+    onSavePlaylist,
+    tapeBackgroundColor,
+    onDelete,
+    queryParam,
+    googleId,
+  } = props;
 
-    const { isAuthenticated, authenticateUser, location, searchResults, onChange, onSearch, onPlayVideo, onReady, onPauseVideo, onResultClick, playing, selectedResult, tapeImages, builderImage, selectImage, tapeLabel, onLabelChange, onPassToSideA, sideA, onPassToSideB, sideB, displayImageSelector, onSaveImage, onDeckSideA, onDeckSideB, onSavePlaylist, tapeBackgroundColor, onDelete, queryParam, googleId } = props;
-   
-    return (
-            <section className="route-section">
-                <Switch location={location}>
-                console.log(location);
-                    <Route exact path="/" component={Login} />
-                    
-                    <Route path='/create-mixtapes'
-                    render={(props) => isAuthenticated ? (<CreateMixtapes {...props} searchResults={searchResults} authenticateUser={authenticateUser} onReady={onReady} onSearch={onSearch} onChange={onChange} onPauseVideo={onPauseVideo} onPlayVideo={onPlayVideo} onResultClick={onResultClick} playing={playing} selectedResult={selectedResult} tapeImages={tapeImages} builderImage={builderImage} selectImage={selectImage} tapeLabel={tapeLabel} onLabelChange={onLabelChange} onPassToSideA={onPassToSideA} sideA={sideA} onPassToSideB={onPassToSideB} sideB={sideB} displayImageSelector={displayImageSelector} onSaveImage={onSaveImage} onSavePlaylist={onSavePlaylist} tapeBackgroundColor={tapeBackgroundColor} onDelete={onDelete} />) : (<Login {...props} />) } />
+  return (
+    <section className="route-section">
+      <Switch location={location}>
+        console.log(location);
+        <Route exact path="/" component={Login} />
 
-                    <Route path='/mixtape-player'
-                    render={(props) => <MixtapePlayer {...props} onDeckSideA={onDeckSideA} onDeckSideB={onDeckSideB} queryParam={queryParam} googleId={googleId}/>} />
+        <Route
+          path="/create-mixtapes"
+          render={props => (
+            isAuthenticated
+              ? (
+                <CreateMixtapes
+                  {...props}
+                  searchResults={searchResults}
+                  authenticateUser={authenticateUser}
+                  onReady={onReady}
+                  onSearch={onSearch}
+                  onChange={onChange}
+                  onPauseVideo={onPauseVideo}
+                  onPlayVideo={onPlayVideo}
+                  onResultClick={onResultClick}
+                  playing={playing}
+                  selectedResult={selectedResult}
+                  tapeImages={tapeImages}
+                  builderImage={builderImage}
+                  selectImage={selectImage}
+                  tapeLabel={tapeLabel}
+                  onLabelChange={onLabelChange}
+                  onPassToSideA={onPassToSideA}
+                  sideA={sideA}
+                  onPassToSideB={onPassToSideB}
+                  sideB={sideB}
+                  displayImageSelector={displayImageSelector}
+                  onSaveImage={onSaveImage}
+                  onSavePlaylist={onSavePlaylist}
+                  tapeBackgroundColor={tapeBackgroundColor}
+                  onDelete={onDelete}
+                />
+              )
+              : (<Login {...props} />))}
+        />
 
-                    <Route path='/mixtape-player/:id' component={MixtapePlayer}
-                    render={(props) => <MixtapePlayer {...props} onDeckSideA={onDeckSideA} onDeckSideB={onDeckSideB} queryParam={queryParam}/>} />
+        <Route
+          path="/mixtape-player"
+          render={props => (
+            <MixtapePlayer
+              {...props}
+              onDeckSideA={onDeckSideA}
+              onDeckSideB={onDeckSideB}
+              queryParam={queryParam}
+              googleId={googleId}
+            />
+          )}
+        />
 
-                    <Route path='/login'
-                    render={(props) => <Login {...props} />} />
+        <Route
+          path="/mixtape-player/:id"
+          component={MixtapePlayer}
+          render={props => (
+            <MixtapePlayer
+              {...props}
+              onDeckSideA={onDeckSideA}
+              onDeckSideB={onDeckSideB}
+              queryParam={queryParam}
+            />
+          )}
+        />
 
-                </Switch>
-            </section>
-    );
+        <Route
+          path="/login"
+          render={props => <Login {...props} />}
+        />
+
+      </Switch>
+    </section>
+  );
 }
 
 // <Route path="/create-mixtapes" component={CreateMixtapes} searchResults={searchResults} />
--- src/components/CreateMixtapes.jsx
+++ src/components/CreateMixtapes.jsx
@@ -7,26 +7,82 @@ import PlaylistBuilderList from './PlaylistBuilderList.jsx';
 import SearchPlayer from './SearchPlayer.jsx';
 
 /** CreateMixtapes component renders all components associated with creation of a mixtape, including
- * PlaylistImageSelector, Search, SearchPlayer, SearchList, and SearchBuilderList at the 
+ * PlaylistImageSelector, Search, SearchPlayer, SearchList, and SearchBuilderList at the
  * create-mixtapes route and is a child component of Container.
  */
 
 const CreateMixtapes = (props) => {
+  const {
+    authenticateUser,
+    searchResults,
+    onSearch,
+    onChange,
+    onPlayVideo,
+    onPauseVideo,
+    onReady,
+    onResultClick,
+    playing,
+    selectedResult,
+    tapeImages,
+    builderImage,
+    selectImage,
+    tapeLabel,
+    onLabelChange,
+    onPassToSideA,
+    sideA,
+    onPassToSideB,
+    sideB,
+    displayImageSelector,
+    onSaveImage,
+    onSavePlaylist,
+    tapeBackgroundColor,
+    onDelete,
+    queryParam,
+  } = props;
 
-    const { authenticateUser, searchResults, onSearch, onChange, onPlayVideo, onPauseVideo, onReady, onResultClick, playing, selectedResult, tapeImages, builderImage, selectImage, tapeLabel, onLabelChange, onPassToSideA, sideA, onPassToSideB, sideB, displayImageSelector, onSaveImage, onSavePlaylist, tapeBackgroundColor, onDelete, queryParam } = props;
-  
-    return (
-        <div style={{marginTop: '4rem'}}>
-            {displayImageSelector ? <PlaylistImageSelector tapeImages={tapeImages} selectImage={selectImage} tapeLabel={tapeLabel} onLabelChange={onLabelChange} onSaveImage={onSaveImage} builderImage={builderImage} tapeBackgroundColor={tapeBackgroundColor} /> :
-        <div className="mx-auto" style={{maxWidth: '860px'}}>
+  return (
+    <div style={{ marginTop: '4rem' }}>
+      {displayImageSelector
+        ? (
+          <PlaylistImageSelector
+            tapeImages={tapeImages}
+            selectImage={selectImage}
+            tapeLabel={tapeLabel}
+            onLabelChange={onLabelChange}
+            onSaveImage={onSaveImage}
+            builderImage={builderImage}
+            tapeBackgroundColor={tapeBackgroundColor}
+          />
+        )
+        : (
+          <div className="mx-auto" style={{ maxWidth: '860px' }}>
             <Search onSearch={onSearch} onChange={onChange} />
-            <SearchPlayer onPlayVideo={onPlayVideo} onReady={onReady} onPauseVideo={onPauseVideo} playing={playing} searchResults={searchResults} selectedResult={selectedResult} onPassToSideA={onPassToSideA} onPassToSideB={onPassToSideB} />
-            <SearchList searchResults={searchResults} onResultClick={onResultClick}/>
-            <PlaylistBuilderList builderImage={builderImage} tapeLabel={tapeLabel} sideA={sideA} sideB={sideB} onSaveImage={onSaveImage} onSavePlaylist={onSavePlaylist} onDelete={onDelete} queryParam={queryParam}/>
-        </div>}
-        </div>
-    )
-}
+            <SearchPlayer
+              onPlayVideo={onPlayVideo}
+              onReady={onReady}
+              onPauseVideo={onPauseVideo}
+              playing={playing}
+              searchResults={searchResults}
+              selectedResult={selectedResult}
+              onPassToSideA={onPassToSideA}
+              onPassToSideB={onPassToSideB}
+            />
+            <SearchList searchResults={searchResults} onResultClick={onResultClick} />
+            <PlaylistBuilderList
+              builderImage={builderImage}
+              tapeLabel={tapeLabel}
+              sideA={sideA}
+              sideB={sideB}
+              onSaveImage={onSaveImage}
+              onSavePlaylist={onSavePlaylist}
+              onDelete={onDelete}
+              queryParam={queryParam}
+            />
+          </div>
+        )}
+    </div>
+  );
+};
 
 // <SearchList searchResults={searchResults} onPlayVideo={onPlayVideo} onReady={onReady} />
 
