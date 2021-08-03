4c324e0350b3589d777d19f63178890326425f33
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-08-03 14:36:32 -0500

    importing models for lyric routes

--- server/routes/lyricRoutes.js
+++ server/routes/lyricRoutes.js
@@ -1,5 +1,7 @@
 const express, { Router } = require('express')
 const lyricRoutes = Router();
+const { User, Playlist } = require('../../database/index')
+
 
 const { fetchLyrics, fetchSongDetails } = require('../API/genius')
 const { explicitSearch } = require('../API/musixmatch')
