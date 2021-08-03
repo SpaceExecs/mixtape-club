f03b588c0b21d3b6aac0e9b0522d6d9bb6ada0cd
berhane tendai cole <71781247+berhanecole@users.noreply.github.com>
2021-08-03 12:48:57 -0500

    musixmatch returns boolean

--- server/API/musixmatch.js
+++ server/API/musixmatch.js
@@ -6,11 +6,20 @@ const explicitSearch = (song, artist) => {
 // https://api.musixmatch.com/ws/1.1/track.search?&q_track=get%20lucky&quorum_factor=1&apikey=d14ac6e33599ee3dcfcf4dd6fdb277fd
 // https://api.musixmatch.com/ws/1.1/track.search?&q_track=get%20lucky&q_artist=Daft Punk&apikey=d14ac6e33599ee3dcfcf4dd6fdb277fd&page_size=1
 const numResults = 1
-const request = `https://api.musixmatch.com/ws/1.1/track.search?&q_track=${song}&q_artist=${artist}&apikey=${MUSIXMATCH_KEY}&page_size=${numResults}`
+const request = `https://api.musixmatch.com/ws/1.1/track.search?&q_track=${song}&q_artist=${artist}&apikey=${process.env.MUSIXMATCH_KEY}&page_size=${numResults}`
 
-
+axios.get(request)
+.then(( { data }) => {
+  const track = data.message.body.track_list[0].track
+  console.log(track);
+  console.log(!!track.explicit)
+  return !!track.explicit
+})
+.catch((err) => console.log(err))
 }
 
+explicitSearch('Power', 'Kanye West')
 
-export default explicitSearch;
+// export default explicitSearch;
+module.exports = { explicitSearch };
 
