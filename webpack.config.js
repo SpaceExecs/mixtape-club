81a0111653d9c3512e69616477fe0dc45af2305e
Caleb Roberts <joesevario@gmail.com>
2021-08-03 16:15:35 -0500

    minor tweak to webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,5 @@
-const path = require("path");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+import path from "path";
+import HtmlWebpackPlugin from "html-webpack-plugin";
 
 const SRC_DIR = path.join(__dirname, "/src");
 const DIST_DIR = path.join(__dirname, "/dist");
