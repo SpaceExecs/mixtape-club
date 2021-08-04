c24f559721da3646ed69ab763ea7f0d0c0d64853
Ubuntu <ubuntu@ip-172-31-5-179.us-east-2.compute.internal>
2021-08-04 16:53:10 +0000

    what

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -38,7 +38,7 @@ module.exports = {
       },
       ssh_options: "StrictHostKeyChecking=no",
       'pre-deploy-local': '',
-      'post-deploy' : 'npm install',
+      'post-deploy' : 'npm run restart',
       'pre-setup': ''
     }
   }
